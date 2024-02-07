8194e69d2e2f6ec3222d74ff726fa81bfbd67b17
Jan <j.dolkowski@soldevelo.com>
2024-02-07 11:08:53 +0100

    CM-487: remove unnecessary payment_cycle mutation

--- payment_cycle/gql_mutations.py
+++ payment_cycle/gql_mutations.py
@@ -4,7 +4,6 @@ from django.utils.translation import gettext as _
 
 from core.schema import OpenIMISMutation
 from payment_cycle.apps import PaymentCycleConfig
-from payment_cycle.models import PaymentCycleMutation, PaymentCycle
 from payment_cycle.services import BenefitPlanPaymentCycleService
 
 
@@ -18,14 +17,7 @@ class ProcessBenefitPlanPaymentCycleMutation(OpenIMISMutation):
 
     @classmethod
     def async_mutate(cls, user, **data):
-        client_mutation_id = data.pop('client_mutation_id', None)
         if not user.has_perms(PaymentCycleConfig.gql_mutation_process_payment_cycle_perms):
             raise PermissionDenied(_("unauthorized"))
         res = BenefitPlanPaymentCycleService(user).process(**data)
-        if client_mutation_id and res['success']:
-            payment_cycle_id = res['data']['id']
-            payment_cycle = PaymentCycle.objects.filter(id=payment_cycle_id)
-            PaymentCycleMutation.object_mutated(
-                user, client_mutation_id=client_mutation_id, payment_cycle=payment_cycle
-            )
         return res if not res['success'] else None
--- payment_cycle/models.py
+++ payment_cycle/models.py
@@ -8,14 +8,3 @@ class PaymentCycle(HistoryModel):
     run_year = models.IntegerField()
     run_month = models.SmallIntegerField()
     type = models.ForeignKey(ContentType, on_delete=models.DO_NOTHING, null=True, unique=False)
-
-
-
-class PaymentCycleMutation(UUIDModel, ObjectMutation):
-    payment_cycle = models.ForeignKey(PaymentCycle, models.DO_NOTHING, related_name='mutations')
-    mutation = models.ForeignKey(
-        MutationLog, models.DO_NOTHING, related_name='payment_cycle')
-
-    class Meta:
-        managed = True
-        db_table = "payment_cycle_PaymentCycleMutation"
